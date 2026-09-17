191d7f91372644ed481eb446d06d4cb52a7526b5
Dhairya Mehra <dhairyamehra2131@gmail.com>
2026-09-17 11:18:04 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (75.24%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.java
+++ 1293-three-consecutive-odds/three-consecutive-odds.java
@@ -3,6 +3,7 @@ class Solution {
     for(int i = 0 ;i+2<arr.length;i++){
         if(arr[i]%2 !=0 && arr[i+1]%2!=0 && arr[i+2]%2 != 0){
             return true;
+            
         }
     }
     return false;
